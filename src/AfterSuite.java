d0e7e554aba7cdd1c204ba4bd9c06fea105cf4cf
ilyaKhim <ilya.himchenko@yandex.ru>
2019-07-04 17:26:14 +0300

    done. with exception. idk why (?)

--- src/AfterSuite.java
+++ src/AfterSuite.java
@@ -5,6 +5,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Test {
+public @interface AfterSuite {
     int priority() default 5;
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -1,20 +1,59 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.*;
 
 public class TestClass {
-    public static void start(Class test) throws NoSuchMethodException {
-        test = Main.class;
 
-        Method m = Main.class.getDeclaredMethod("printWelcome", String.class);
-        m.setAccessible(true);
-        Test test1 = m.getAnnotation(Test.class);
-        System.out.println(test1.value());
+
+    public static void start(Class c) throws InvocationTargetException, IllegalAccessException {
+
+
+        Method[] methods = c.getDeclaredMethods();
+        List<Method> list = new ArrayList<>();
+
+        for (Method o : methods) {
+            if (o.isAnnotationPresent(Test.class)) {
+                int prio = o.getAnnotation(Test.class).priority();
+                if (prio < 1 || prio > 10) throw new RuntimeException("Ошибка приоритета");
+                list.add(o);
+            }
+        }
+
+        list.sort(new Comparator<Method>() {
+            @Override
+            public int compare(Method o1, Method o2) {
+                return o2.getAnnotation(Test.class).priority() - o1.getAnnotation(Test.class).priority();
+            }
+        });
+
+
+
+        for (Method o : methods) {
+            if (o.isAnnotationPresent(BeforeSuite.class)) {
+                if (list.get(0).isAnnotationPresent(BeforeSuite.class))
+                    throw new RuntimeException("BeforeSuite exception");
+                list.add(0, o);
+            }
+            if (o.isAnnotationPresent(AfterSuite.class)){
+                if (list.get(0).isAnnotationPresent(AfterSuite.class)){
+                    throw new RuntimeException("AfterSuite exception");
+                }
+                    list.add(o);
+
+            }
+        }
+
+        for(Method o: methods){
+
+        }
+        for( Method u: list){
+            u.invoke(null);
+        }
+
+
+
     }
-
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    public static void main (String[]args) throws InvocationTargetException, IllegalAccessException {
         start(Main.class);
-
     }
-
 }
